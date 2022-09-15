3a8fc2c9102d01fae7b23c193aab33e67e0cede8
Dennis Kimathi <kimathidennis38@gmail.com>
2022-09-15 16:21:33 +0300

    fix: add more sections

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,29 +1,18 @@
-import React, { useState } from "react";
-import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
-import { MdVideoCameraFront } from "react-icons/md";
-import { meal } from "../../constants";
+import React from 'react';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 
-import "./Intro.css";
+import { meal } from '../../constants';
+import './Intro.css';
 
 const Intro = () => {
-  const [playVideo, setPlayVideo] = useState(false);
+  const [playVideo, setPlayVideo] = React.useState(false);
   const vidRef = React.useRef();
-
-  const handleVideo = () => {
-    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
-
-    if (playVideo) {
-      vidRef.current.pause();
-    } else {
-      vidRef.current.play();
-    }
-  };
 
   return (
     <div className="app__video">
       <video
-        src={meal}
         ref={vidRef}
+        src={meal}
         type="video/mp4"
         loop
         controls={false}
@@ -32,45 +21,14 @@ const Intro = () => {
       <div className="app__video-overlay flex__center">
         <div
           className="app__video-overlay_circle flex__center"
-          onClick={handleVideo}
-        >
-          {playVideo ? (
-            <BsPauseFill color="#fff" fontSize={30} />
-          ) : (
-            <BsFillPlayFill color="#fff" fontSize={30} />
-          )}
-        </div>
-      </div>
-    </div>
-  );
-};
-
-  const vidRef = React.useRef();
-
-  const handleVideo = () => {
-    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
-
-    if (playVideo) {
-      vidRef.current.pause();
-    } else {
-      vidRef.current.play();
-    }
-  };
-
-  return (
-    <div className="app__video">
-      <video
-        src={meal}
-        ref={vidRef}
-        type="video/mp4"
-        loop
-        controls={false}
-        muted
-      />
-      <div className="app__video-overlay flex__center">
-        <div
-          className="app__video-overlay_circle flex__center"
-          onClick={handleVideo}
+          onClick={() => {
+            setPlayVideo(!playVideo);
+            if (playVideo) {
+              vidRef.current.pause();
+            } else {
+              vidRef.current.play();
+            }
+          }}
         >
           {playVideo ? (
             <BsPauseFill color="#fff" fontSize={30} />
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,29 +1,31 @@
-import React from "react";
+import React from 'react';
 
-import { images, data } from "../../constants";
-import { SubHeading, MenuItem } from "../../components";
-import "./SpecialMenu.css";
+import { SubHeading, MenuItem } from '../../components';
+import { data, images } from '../../constants';
+import './SpecialMenu.css';
 
 const SpecialMenu = () => (
   <div className="app__specialMenu flex__center section__padding" id="menu">
     <div className="app__specialMenu-title">
-      <SubHeading title="Menu that fits you palatte" />
-      <h1 className="headtext__cormorant">Today's Special</h1>
+      <SubHeading title="Menu that fits your palatte" />
+      <h1 className="headtext__cormorant">Today&apos;s Special</h1>
     </div>
+
     <div className="app__specialMenu-menu">
-      <div className="app__specialMenu-menu_wine flex__center">
+      <div className="app__specialMenu-menu_wine  flex__center">
         <p className="app__specialMenu-menu_heading">Wine & Beer</p>
         <div className="app__specialMenu_menu_items">
           {data.wines.map((wine, index) => (
-           <MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags} />
+            <MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags} />
           ))}
         </div>
       </div>
+
       <div className="app__specialMenu-menu_img">
-        <img src={images.menu} alt="menu img" />
+        <img src={images.menu} alt="menu__img" />
       </div>
 
-      <div className="app__specialMenu-menu_cocktails flex__center">
+      <div className="app__specialMenu-menu_cocktails  flex__center">
         <p className="app__specialMenu-menu_heading">Cocktails</p>
         <div className="app__specialMenu_menu_items">
           {data.cocktails.map((cocktail, index) => (
@@ -33,7 +35,7 @@ const SpecialMenu = () => (
       </div>
     </div>
 
-    <div style={{ marginTop: "15px" }}>
+    <div style={{ marginTop: 15 }}>
       <button type="button" className="custom__button">View More</button>
     </div>
   </div>
